2b711523a7d5c5132fd2383f737bf30a91f88da0
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-11-13 17:25:13 +0800

    update js

--- index.js
+++ index.js
@@ -1,43 +1,137 @@
-//console.log("hello world")
+// //console.log("hello world")
 
 
 
-//layout of tic tac toe
-function Gameboard(){
+// function Gameboard(){
+//     const rows = 3;
+//     const columns = 3;
+//     const board = [];
+
+//    for (let i = 0; i < rows; i++){
+//     board[i] = [];
+//     for (let j = 0; j < columns; j++){
+//         board[i].push(cell())
+//     }
+//    }
+
+//    const getBoard = () => board;
+
+//    const dropToken = (column, player) =>{
+//     const availableCells = board
+//     .filter((row) => row[column].getValue() === 0)
+//     .map(row => row[column]);
+
+//     if (!availableCells.length) return;
+
+//     const lowestRow = availableCells.length - 1;
+//     board[lowestRow][column].addToken(player);
+
+//     const printBoard = () => {
+//         const boardWithCellValues = board
+//         .map(row => row.map((cell) => cell.getValue()));
+//         console.log(boardWithCellValues);
+//     };
+
+//     return { getBoard, dropToken, printBoard };
+
+//    };
+
+// };
+
+
+// function Cell(){
+//     let value = 0;
+
+//     const addToken = (player) => {
+//         value = player;
+//     }
+
+//     const getValue = () => value;
+
+//     return { addToken, getValue };
+// }
+
+
+// function GameController(
+//     playerOneName = "Player One",
+//     playerTwoName = "Player Two"
+// ){
+//     const board = Gameboard();
+
+//     const players = [
+//         {
+//             name: playerOneName,
+//             token: 1
+//         },
+//         {
+//             name: playerTwoName,
+//             token: 2
+//         }
+//     ];
+
+//     let activePlayer = players[0];
+
+//     const switchPlayerTurn = () => {
+//         activePlayer = activePlayer === players[0] ? players[1] : players[0];
+//     };
+//     const getActivePlayer = () => activePlayer;
+
+//     const printNewRound = () => {
+//         board.printBoard();
+//         console.log(`${getActivePlayer().name}'s turn.`);
+//     }
+
+//     switchPlayerTurn()
+//     printNewRound()
+
+//     return{playRound, getActivePlayer}
+// }
+
+// const game = GameController()
+
+
+
+
+
+function gameBoard(){
+    const board = [];
     const rows = 3;
     const columns = 3;
-    const board = [];
 
-   for (let i = 0; i < rows; i++){
-    board[i] = [];
-    for (let j = 0; j < columns; j++){
-        board[i].push(cell())
-    }
-   }
-
-   const getBoard = () => board;
-
-   const dropToken = (column, player) =>{
-    const availableCells = board
-    .filter((row) => row[column].getValue() === 0)
-    .map(row => row[column]);
-
-    if (!availableCells.length) return;
-
-    const lowestRow = availableCells.length - 1;
-    board[lowestRow][column].addToken(player);
-
-    const printBoard = () => {
-        const boardWithCellValues = board
-        .map(row => row.map((cell) => cell.getValue()));
-        console.log(boardWithCellValues);
+//2d array
+    for(let i = 0; i < rows; i ++){
+        board[i] = [];
+        for(let j = 0; i< columns; j ++){
+            board[i].push(Cell());
+        };
     };
 
-    return { getBoard, dropToken, printBoard };
+    const getBoard = () => board;
 
-   };
+    // const placeMarker = (column, player) =>{
+    //     const availableCells = board.filter((row) => row[column].getValue()===0).map(row => row[column]);
+    // };
 
-};
+    const placeMarker = (row, column, player) => {
+        const cell = board[row][column];
+
+        if(cell.getValue() !== 0) return false;
+        cell.addToken(player);
+        return true;
+
+    }
+
+    const resetBoard = () => {
+        for(let i = 0; i < rows; i ++){
+            board[i] = [];
+            for(let j = 0; j< columns; j ++){
+                board[i][j] = Cell();
+            };
+    };
+    };
+
+    return {getBoard, placeMarker, resetBoard};
+}
 
 
 function Cell(){
@@ -45,58 +139,68 @@ function Cell(){
 
     const addToken = (player) => {
         value = player;
-    }
+    };
 
     const getValue = () => value;
 
-    return { addToken, getValue };
+    return {addToken, getValue}
 }
 
 
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-){
-    const board = Gameboard();
+
+// function player(name, marker){
+//     const getName = () => name;
+//     const getMarker = () => marker;
+    
+//     return {getName, getMarker};
+// }
+
+
+
+
+function gameController(player1Name = "Player One", player2Name = "Player Two")
+{
+    const board = gameBoard();
 
     const players = [
         {
-            name: playerOneName,
+            name: player1Name,
             token: 1
         },
         {
-            name: playerTwoName,
+            name: player2Name,
             token: 2
         }
     ];
 
     let activePlayer = players[0];
+    
+    // const switchPlayer = () => {
+    //     if (activePlayer === player1){
+    //         player1
+    //     } else{
+    //         activePlayer === player2
+    //     }
+    // }
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
+
     const getActivePlayer = () => activePlayer;
 
     const printNewRound = () => {
         board.printBoard();
         console.log(`${getActivePlayer().name}'s turn.`);
+
+    };
+
+    const playRound = (column) => {
+        console.log(`Dropping ${getActivePlayer().name}'s token into column ${column}`)
+    
+        board.dropToken(column, getActivePlayer().token);
     }
+
+    return {switchPlayerTurn, playRound}
+
 }
-
-
-//player & computer
-// const player = (function(){
-//     let score = 0;
-//     let playRound = 0;
-
-//     for(let i = 0; i < playRound; i++){
-//         return score;
-//     }
-    
-    
-
-// })();
-
-
-
-console.log(player)
